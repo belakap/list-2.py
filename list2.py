111e15e2edd3d0d3052abdc13a0a623e6e5dca63
ngomba <belakap24@gmail.com>
2024-08-16 10:08:17 -0400

    list2.py

--- list2.py
+++ list2.py
@@ -1,32 +1,32 @@
-# Additional basic list exercises
-
-# D. Given a list of numbers, return a list where
-# all adjacent == elements have been reduced to a single element,
-# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
-# modify the passed in list.
-
 def remove_adjacent(nums):
-    # +++your code here+++
+    result = []
+    for num in nums:
+        if not result or result[-1] != num:
+            result.append(num)
     return
 
-
-# E. Given two lists sorted in increasing order, create and return a merged
-# list of all the elements in sorted order. You may modify the passed in lists.
-# Ideally, the solution should work in "linear" time, making a single
-# pass of both lists.
 
 def linear_merge(list1, list2):
-    # +++your code here+++
-    return
+    i, j = 0, 0
+    merged_list = []
+    
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged_list.append(list1[i])
+            i += 1
+        else:
+            merged_list.append(list2[j])
+            j += 1
+    while i < len(list1):
+        merged_list.append(list1[i])
+        i += 1
+    
+    while j < len(list2):
+        merged_list.append(list2[j])
+        j += 1
+    
+    return merged_list
 
-
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
-# is not constant time with the standard python list implementation, so
-# the above is not strictly linear time.
-# An alternate approach uses pop(-1) to remove the endmost elements
-# from each list, building a solution list which is backwards.
-# Then use reversed() to put the result back in the correct order. That
-# solution works in linear time, but is more ugly.
 
 
 # Simple provided test() function used in main() to print
